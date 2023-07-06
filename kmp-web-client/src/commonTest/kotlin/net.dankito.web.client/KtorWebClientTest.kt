8f2e7f0015f755103a7525fc5a2c405782876db3
dankito <git@dankito.net>
2023-07-06 12:09:37 +0200

    Implemented KtorWebClientTest

---
+++ kmp-web-client/src/commonTest/kotlin/net.dankito.web.client/KtorWebClientTest.kt
@@ -0,0 +1,56 @@
+package net.dankito.web.client
+
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
+class KtorWebClientTest {
+
+    companion object {
+        private val Url = "https://www.nytimes.com" // does not work for JS Browser, i guess due to CORS
+    }
+
+
+    private val underTest = KtorWebClient(ignoreCertificateErrors = true)
+
+
+    @Test
+    fun get() = runTest {
+        val response = underTest.getAsync<String>(Url)
+
+        assertTrue(response.successful)
+        assertEquals(200, response.statusCode)
+        assertNotNull(response.body)
+    }
+
+    @Test
+    fun getWithParameters() = runTest {
+        val response = underTest.getAsync(RequestParameters(Url, String::class))
+
+        assertTrue(response.successful)
+        assertEquals(200, response.statusCode)
+        assertNotNull(response.body)
+    }
+
+
+    @Test
+    fun head() = runTest {
+        val response = underTest.headAsync(Url)
+
+        assertTrue(response.successful)
+        assertEquals(200, response.statusCode)
+        assertNotNull(response.body)
+    }
+
+    @Test
+    fun headWithParameters() = runTest {
+        val response = underTest.headAsync(RequestParameters(Url, Unit::class))
+
+        assertTrue(response.successful)
+        assertEquals(200, response.statusCode)
+        assertNotNull(response.body)
+    }
+
+}
