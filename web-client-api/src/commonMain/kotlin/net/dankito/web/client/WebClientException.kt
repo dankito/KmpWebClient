1e4b053d84084c45221e89bbff6d206f6fc04e0c
dankito <git@dankito.net>
2025-04-17 01:47:32 +0200

    Added cause and characterization of error

--- web-client-api/src/commonMain/kotlin/net/dankito/web/client/WebClientException.kt
+++ web-client-api/src/commonMain/kotlin/net/dankito/web/client/WebClientException.kt
@@ -1,12 +1,29 @@
 package net.dankito.web.client
 
+import io.ktor.client.network.sockets.*
+import io.ktor.utils.io.errors.*
+
 open class WebClientException(
     open val httpStatusCode: Int,
-    errorMessage: String
-) : Exception(errorMessage) {
+    errorMessage: String,
+    cause: Throwable? = null
+) : Exception(errorMessage, cause) {
+
+    val isConnectTimeout = cause is ConnectTimeoutException
+
+    val isSocketTimeout = cause is SocketTimeoutException
+
+    val isIOException = cause is IOException
+
+    val isNetworkError = isConnectTimeout || isSocketTimeout || isIOException
+
+    val isClientError = httpStatusCode in 400..499
+
+    val isServerError = httpStatusCode in 500..599
+
 
     override fun toString(): String {
-        return "$httpStatusCode $message"
+        return "$httpStatusCode $message. isNetworkError = $isNetworkError, isClientError = $isClientError, isServerError = $isServerError"
     }
 
 }
