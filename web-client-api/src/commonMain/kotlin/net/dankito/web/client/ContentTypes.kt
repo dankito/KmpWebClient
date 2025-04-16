32672349e7f18851f7122738a8da132ca29a3f14
dankito <git@dankito.net>
2025-04-17 01:42:36 +0200

    Fixed Content Types and added additional ones

--- web-client-api/src/commonMain/kotlin/net/dankito/web/client/ContentTypes.kt
+++ web-client-api/src/commonMain/kotlin/net/dankito/web/client/ContentTypes.kt
@@ -2,10 +2,12 @@ package net.dankito.web.client
 
 object ContentTypes {
 
-    const val JSON = "application/x-www-form-urlencoded; charset=UTF-8"
-    const val XML = "application/json; charset=UTF-8"
-    const val FORM_URL_ENCODED = "application/xml; charset=UTF-8"
+    const val JSON = "application/json; charset=UTF-8"
+    const val XML = "application/xml; charset=UTF-8"
+    const val FORM_URL_ENCODED = "application/x-www-form-urlencoded; charset=UTF-8"
     const val MULTIPART = "multipart/form-data; charset=UTF-8"
     const val PLAIN_TEXT = "text/plain; charset=UTF-8"
+    const val OCTET_STREAM = "application/octet-stream"
+    const val PDF = "application/pdf"
 
 }
