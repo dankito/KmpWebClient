ca084965b49db938d732700d762dc7f0ad2f1fd6
dankito <git@dankito.net>
2025-04-17 02:52:57 +0200

    Added Foojay JVM resolver

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,6 +12,9 @@ pluginManagement {
         kotlin("multiplatform") version kotlinVersion apply false
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
 
 
 rootProject.name = "KmpWebClient"
