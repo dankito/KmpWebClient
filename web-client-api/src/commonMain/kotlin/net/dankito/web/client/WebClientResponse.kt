acb9162a50ee5f6cf258661ab7c09a2efe5da681
dankito <git@dankito.net>
2025-04-18 15:00:28 +0200

    Moved response based properties to ResponseDetails

--- web-client-api/src/commonMain/kotlin/net/dankito/web/client/WebClientResponse.kt
+++ web-client-api/src/commonMain/kotlin/net/dankito/web/client/WebClientResponse.kt
@@ -10,48 +10,6 @@ open class WebClientResponse<T>(
 
     val statusCode = responseDetails?.statusCode ?: -1
 
-    open fun getHeaderValue(headerName: String): String? {
-        val headerNameLowerCased = headerName.lowercase() // header names are case insensitive, so compare them lower cased
-
-        responseDetails?.headers?.let { headers ->
-            headers.keys.forEach {
-                if(it.lowercase() == headerNameLowerCased) {
-                    return headers[it]?.firstOrNull()
-                }
-            }
-        }
-
-        return null
-    }
-
-
-    open val isInformationalResponse: Boolean
-        get() = statusCode >= 100 && statusCode < 200
-
-    open val isSuccessResponse: Boolean
-        get() = statusCode >= 200 && statusCode < 300
-
-    open val isRedirectionResponse: Boolean
-        get() = statusCode >= 300 && statusCode < 400
-
-    open val isClientErrorResponse: Boolean
-        get() = statusCode >= 400 && statusCode < 500
-
-    open val isServerErrorResponse: Boolean
-        get() = statusCode >= 500 && statusCode < 600
-
-    open fun containsCookie(cookieName: String): Boolean {
-        return getCookie(cookieName) != null
-    }
-
-    open fun getCookie(cookieName: String): Cookie? {
-        return responseDetails?.cookies?.firstOrNull { cookieName == it.name }
-    }
-
-    open val retryAfterSeconds: Int? by lazy {
-        getHeaderValue("Retry-After")?.toIntOrNull()
-    }
-
 
     override fun toString(): String {
         return if (successful) {
