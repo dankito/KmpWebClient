22b0b9a9ab6c628a67ecc739691a8784bc16f409
dankito <git@dankito.net>
2025-04-18 15:33:17 +0200

    Added documentation for response properties

--- web-client-api/src/commonMain/kotlin/net/dankito/web/client/WebClientResponse.kt
+++ web-client-api/src/commonMain/kotlin/net/dankito/web/client/WebClientResponse.kt
@@ -1,13 +1,47 @@
 package net.dankito.web.client
 
 open class WebClientResponse<T>(
+    /**
+     * Is true if
+     * - a response has been received,
+     * - the HTTP status code is in range 200..299 (= [ResponseDetails.isSuccessResponse] is true),
+     * - the response body could be deserialized.
+     *
+     * So if there's an error during request body deserialization, `successful` is `false` even though
+     * [ResponseDetails.isSuccessResponse] is `true`.
+     */
     open val successful: Boolean,
+
+    /**
+     * The URL that the web client has requested which may be a combination of baseUrl set on WebClient and relative
+     * URL used for request.
+     *
+     * In case of network error may only the (relative) URL used for request, not the URL the web client really has
+     * requested.
+     */
     open val requestedUrl: String,
+
+    /**
+     * In case a response has been retrieved, details of the response like headers, cookies, ...
+     */
     open val responseDetails: ResponseDetails? = null,
-    open val error: Throwable? = null,
+
+    /**
+     * In case an error has occurred, details about the error.
+     */
+    open val error: WebClientException? = null,
+
+    /**
+     * The retrieved (and may deserialized) response body, if any.
+     */
     open val body: T? = null
 ) {
 
+    /**
+     * The HTTP status code of the retrieved response.
+     *
+     * Set to `-1` in case no response has been retrieved.
+     */
     val statusCode = responseDetails?.statusCode ?: -1
 
 
