944d991e89120d6bb2c00c7ddfcdd7649ecdcebc
dankito <git@dankito.net>
2025-04-22 17:35:59 +0200

    Added option to set if used Ktor engine should be logged

--- ktor-web-client/src/commonMain/kotlin/net.dankito.web.client/KtorClientConfiguration.kt
+++ ktor-web-client/src/commonMain/kotlin/net.dankito.web.client/KtorClientConfiguration.kt
@@ -4,6 +4,8 @@ import net.codinux.log.logger
 
 object KtorClientConfiguration {
 
+    var logUsedEngine = false
+
     private val log by logger()
 
 
@@ -11,7 +13,11 @@ object KtorClientConfiguration {
         val supportedEngines = Platform.preferredEngines
 
         return Platform.availableEngines.firstOrNull { supportedEngines.contains(it) }.also {
-            log.debug { "Using HTTP Client Engine ${it?.engineName}" }
+            if (logUsedEngine) {
+                log.info { "Using HTTP Client Engine ${it?.engineName}" }
+            } else {
+                log.debug { "Using HTTP Client Engine ${it?.engineName}" }
+            }
         }
     }
 
