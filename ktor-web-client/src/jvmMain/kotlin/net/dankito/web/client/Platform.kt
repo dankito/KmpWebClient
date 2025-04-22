30345198e7e9b76b79d42cb4fab78253dabbc04c
dankito <git@dankito.net>
2025-04-22 16:52:45 +0200

    Added Platform.availableEngines

--- ktor-web-client/src/jvmMain/kotlin/net/dankito/web/client/Platform.kt
+++ ktor-web-client/src/jvmMain/kotlin/net/dankito/web/client/Platform.kt
@@ -1,9 +1,10 @@
 package net.dankito.web.client
 
 import io.ktor.client.*
-import io.ktor.client.engine.java.*
 
 actual object Platform {
+
+    actual val availableEngines: LinkedHashSet<KtorEngine> = JavaPlatformCommon.availableEngines
 
     actual fun createPlatformSpecificHttpClient(ignoreCertificateErrors: Boolean, config: HttpClientConfig<*>.() -> Unit): HttpClient? =
         JavaPlatformCommon.createPlatformSpecificHttpClient(ignoreCertificateErrors, config)
--- ktor-web-client/src/wasmJsMain/kotlin/net.dankito.web.client/Platform.wasmJs.kt
+++ ktor-web-client/src/wasmJsMain/kotlin/net.dankito.web.client/Platform.wasmJs.kt
@@ -5,6 +5,8 @@ import io.ktor.client.engine.js.*
 
 actual object Platform {
 
+    actual val availableEngines: LinkedHashSet<KtorEngine> = linkedSetOf(KtorEngine.Js)
+
     actual fun createPlatformSpecificHttpClient(ignoreCertificateErrors: Boolean, config: HttpClientConfig<*>.() -> Unit): HttpClient? =
         HttpClient(Js) {
             config(this)
