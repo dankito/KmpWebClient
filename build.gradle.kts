597efa27d7232bf72bbb255f8fae1d073615e23b
dankito <git@dankito.net>
2025-06-20 21:46:04 +0200

    Released version 1.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
 allprojects {
     group = "net.dankito.web"
-    version = "1.5.1-SNAPSHOT"
+    version = "1.5.1"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/KmpWebClient"
