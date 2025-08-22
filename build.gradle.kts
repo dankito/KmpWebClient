cf157b8897240056f1ca04bbf133c0257e702f6f
dankito <git@dankito.net>
2025-08-22 14:58:56 +0200

    Released version 1.6.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
 allprojects {
     group = "net.dankito.web"
-    version = "1.6.1-SNAPSHOT"
+    version = "1.6.1"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/KmpWebClient"
