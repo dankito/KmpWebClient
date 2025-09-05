94c2d5d6af2b579454affa7dd91bb7121ab1dc81
dankito <git@dankito.net>
2025-09-05 12:15:50 +0200

    Bumped version to 1.7.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
 allprojects {
     group = "net.dankito.web"
-    version = "1.7.1"
+    version = "1.7.2-SNAPSHOT"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/KmpWebClient"
