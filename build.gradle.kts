7df5df2ccda2ff72a52b8b3ee7750584a67370ec
dankito <git@dankito.net>
2026-03-23 07:42:08 +0100

    Bumped version to 1.8.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ buildscript {
 
 allprojects {
     group = "net.dankito.web"
-    version = "1.8.0"
+    version = "1.8.1-SNAPSHOT"
 
 
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/dankito/KmpWebClient"
